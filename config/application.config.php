7a3dbdaff443f6da9617cd03967e75076afde677
Vladyslav Bondarenko <wbondarenko@programmer.net>
2015-03-03 00:54:12 +0100

    20150303

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,8 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-          'Application'
+          'EdpModuleLayouts'
+        , 'Application'
         , 'Domain'
         , 'ControlPanel'
     ),
